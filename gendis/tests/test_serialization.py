41174b410d723ee5f205d7af0434321ee09c3a5a
GillesVandewiele <gilles.vandewiele@ugent.be>
2018-08-23 11:03:46 +0200

    small tweaks Small change in notebook Save/load functionality (resolves #3) Remove the data directory (resolves #5)

---
+++ gendis/tests/test_serialization.py
@@ -0,0 +1,15 @@
+from gendis.genetic import GeneticExtractor
+from tslearn.generators import random_walk_blobs
+import numpy as np
+import os
+
+def test_serialization():
+	X, y = random_walk_blobs(n_ts_per_blob=20, sz=64, noise_level=0.1)
+	X = np.reshape(X, (X.shape[0], X.shape[1]))
+	extractor = GeneticExtractor(iterations=5, n_jobs=1, population_size=10)
+	distances = extractor.fit_transform(X, y)
+	extractor.save('temp.p')
+	new_extractor = GeneticExtractor.load('temp.p')
+	new_distances = new_extractor.transform(X)
+	np.testing.assert_array_equal(distances, new_distances)
+	os.remove('temp.p')
